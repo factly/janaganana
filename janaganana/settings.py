2932aac18494c028888d1faa513319de52e4ffb1
mthipparthi <mahesh.thipparthi@gmail.com>
2016-12-14 06:33:38 +1100

    cache added

--- janaganana/settings.py
+++ janaganana/settings.py
@@ -5,7 +5,6 @@ from wazimap.settings import *  # noqa
 INSTALLED_APPS = ['janaganana'] + INSTALLED_APPS
 
 
-
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://factlyin:factlyin@localhost/factlyin')
 DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 DATABASES['default']['ATOMIC_REQUESTS'] = True
@@ -55,3 +54,33 @@ WAZIMAP['tagline'] = 'Make sense of Indian census data'
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'),
 )
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 604800 #60secs x 60minutes x 168hours(1 week)
+CACHE_MIDDLEWARE_KEY_PREFIX = 'jana'
+
+if DEBUG:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': 'unix:/tmp/memcached.sock',
+        }
+    }
+    
