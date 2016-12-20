735ed5239f75199bfcdcb150f8fb28e4baade5ff
Ubuntu <ubuntu@ip-172-31-0-84.ap-south-1.compute.internal>
2016-12-20 19:19:08 +0000

    First Commit

---
+++ guni_config.py
@@ -0,0 +1,9 @@
+raw_env = [
+   'DATABASE_URL=postgresql://factlyin:factlyin@localhost/factlyin',
+   'DJANGO_SECRET_KEY=345826e4-de82-462b-96c3-2fbbc0eb30f3',
+   'DJANGO_DEBUG=False',
+   'DJANGO_SETTINGS_MODULE=janaganana.settings',
+]
+errorlog = '/tmp/gunicorn-error.log'
+accesslog = '/tmp/gunicorn-access.log'
+workers = 3
