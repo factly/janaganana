5b69eeccf4c7a92d2fa49973d4c4eaf3bc2edd96
Shashi Deshetti <deshetti@gmail.com>
2020-09-23 14:17:16 -0500

    modified example.org domains to ci

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(countingindia.com www.countingindia.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
